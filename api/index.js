4799f5952a052f383cac800f298f7b002c3afa40
Dan-Yaacobi <yaacobidan@gmail.com>
2026-04-22 16:33:18 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true });
-}
-// import serverless from 'serverless-http';
-// import { createApp } from '../backend/src/app.js';
+import serverless from 'serverless-http';
+import { createApp } from '../backend/src/app.js';
 
-// const app = createApp();
+const app = createApp();
 
-// export default serverless(app);
+export default serverless(app);
